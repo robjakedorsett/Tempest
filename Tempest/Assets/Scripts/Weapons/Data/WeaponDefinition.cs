9c687112b1e68dc864c890a287caa4ccd28cfd89
Rob Dorsett <rob.dorsett@helitz.com>
2026-05-28 19:06:37 +0100

    feat(weapons): add WeaponDefinition ScriptableObject

---
+++ Tempest/Assets/Scripts/Weapons/Data/WeaponDefinition.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+namespace Tempest.Weapons
+{
+    [CreateAssetMenu(fileName = "NewWeapon", menuName = "Tempest/Weapons/Weapon Definition")]
+    public class WeaponDefinition : ScriptableObject
+    {
+        [Header("Identity")]
+        public string weaponName;
+        [TextArea(2, 4)]
+        public string description;
+        public Sprite icon;
+        public WeaponSlot slot;
+        public WeaponType type;
+
+        [Header("Stats")]
+        public float damage;
+        public float fireRate;
+        public int magazineSize;
+        public float reloadTime;
+        public float range;
+        public float spread;
+
+        [Header("Effects")]
+        public GameObject muzzleFlashPrefab;
+        public GameObject hitEffectPrefab;
+        public AudioClip fireSound;
+        public AudioClip reloadSound;
+
+        [Header("Meta")]
+        public bool unlockedByDefault = true;
+    }
+}
