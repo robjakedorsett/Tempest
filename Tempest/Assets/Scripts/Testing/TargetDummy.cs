341601356c1ca95d9f5e39b08c53c8478ecc430b
Rob Dorsett <rob.dorsett@helitz.com>
2026-05-29 12:03:43 +0100

    feat(testing): add TargetDummy for validating weapon feedback chain

---
+++ Tempest/Assets/Scripts/Testing/TargetDummy.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using Tempest.Weapons;
+using UnityEngine;
+
+namespace Tempest.Testing
+{
+    public class TargetDummy : MonoBehaviour, IDamageable
+    {
+        [SerializeField] private float maxHealth = 100f;
+        [SerializeField] private float respawnDelay = 2f;
+
+        private float _currentHealth;
+        private Collider _collider;
+        private Renderer _renderer;
+
+        private void Awake()
+        {
+            _currentHealth = maxHealth;
+            _collider = GetComponent<Collider>();
+            _renderer = GetComponent<Renderer>();
+        }
+
+        public bool TakeDamage(float damage, Vector3 hitPoint, Vector3 hitNormal)
+        {
+            _currentHealth -= damage;
+            if (_currentHealth <= 0f)
+            {
+                Die();
+                return true;
+            }
+            return false;
+        }
+
+        private void Die()
+        {
+            _collider.enabled = false;
+            _renderer.enabled = false;
+            StartCoroutine(RespawnAfterDelay());
+        }
+
+        private IEnumerator RespawnAfterDelay()
+        {
+            yield return new WaitForSeconds(respawnDelay);
+            _currentHealth = maxHealth;
+            _collider.enabled = true;
+            _renderer.enabled = true;
+        }
+    }
+}
